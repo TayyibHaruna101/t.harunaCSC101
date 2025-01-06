4d650acd5ec7118fc3dfbef3e98232d8522ec432
t.harunaCSC101 <tayyib.haruna@pau.edu.ng>
2025-01-06 22:14:52 +0100

    My nineth commit

--- week-8/practice-8/src/main.rs
+++ week-8/practice-8/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     let mut mountain_heights = ("Everest", 8848, "Fishtail", 6993);
     println!("Original tuple = {:?}", mountain_heights);
     mountain_heights.2 = "Lhotse";
-    mountain_heights.3 = 8517;
+    mountain_heights.3 = 8516;
 
     println!("Changed tuple = {:?}", mountain_heights);
 }
